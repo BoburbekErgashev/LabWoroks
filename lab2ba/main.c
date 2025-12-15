70dbc3e8a5706843eb0346f16a79d991d2af5486
ErgashevBoburbek <odilovabdulaziz2007@gmail.com>
2025-12-15 22:04:28 +0500

    Added lab2ba

--- lab2ba/main.c
+++ lab2ba/main.c
@@ -9,90 +9,67 @@ typedef struct st_list{
     struct st_list *next;
 }st_list;
 
-
-typedef struct file_list{
-    FILE *file_pointer;
-    char filename[30];
-    st_list *head;
-    struct file_list *next;
-}file_list;
-
-
-file_list *opened_files = NULL;
-
-
-void read_function(char filename[30]){
-    FILE *fptr = fopen(filename, "r");
-    file_list *temp = malloc(sizeof(file_list));
-    temp->file_pointer = fptr;
-    strcpy(temp->filename, filename);
-    temp->next = NULL;
-    if (opened_files == NULL){
-        opened_files = temp;
-    }else{
-        file_list *current = opened_files;
-        while (current->next != NULL){
-           current = current->next;
-        }
-        current->next = temp;
-    }
-}
-
-
-int check_files(char filename[30]){
-    file_list *temp = opened_files;
-    if(opened_files == NULL){
-        return 0;
-    }else{
-        while(temp != NULL){
-            if(strcmp(filename, temp->filename) == 0){
-                return 1;
-            }
-            temp = temp->next;
-        }
-        return 0;
-    }
-}
-
+st_list *student_l = NULL;
 
 void read_file(char filename[30]){
-    if(check_files(filename) == 0){
-        read_function(filename);
-    }
-}
-
-
-st_list *find_stl(char filename[30]){
-    file_list *temp = opened_files; 
-    while(temp !=NULL){
-        if(strcmp(filename, temp->filename) == 0){
-            return temp->head;
+    FILE *fptr = fopen(filename, "r");
+    char stname[30] = {};
+    int mark;
+    st_list *temp;
+    while(fscanf(fptr, "%s %d", stname, &mark) != EOF){
+        temp = malloc(sizeof(st_list));
+        strcpy(temp->name, stname);
+        temp->mark = mark;
+        if(student_l == NULL){
+            temp->next = NULL;
+            student_l = temp;
+        }else{
+            temp->next = student_l;
+            student_l = temp;
         }
-        temp = temp->next;
     }
 }
 
+void name_sort(){
+    st_list *current, *previous, *temp;
+    int swapped = 0;
+    do{
+        swapped = 0;
+        current = student_l;
+        previous = NULL;
+        while(current != NULL && current->next != NULL){
+            if(strcmp(current->name, current->next->name) > 0){
+                if(previous == NULL){
+                    temp = current->next->next;
+                    current->next->next = current;
+                    previous = current->next;
+                    student_l = current->next;
+                    current->next = temp;
+                    swapped = 1;
+                }else{
+                    temp = current->next->next;
+                    current->next->next = current;
+                    previous->next = current->next;
+                    previous = current->next;
+                    current->next = temp;
+                    swapped = 1;
+                }
+            }else{
+                previous = current;
+                current = current->next;
+            }
+        }
+
+    }while(swapped == 1);
+}
 
 int main(){
-    int a;
-    char file[30];
-    while(1){
-        printf("\nEnter action\n");
-        scanf("%d", &a);
-        if(a == 1){
-            printf("\nEnter  filename to check if it is exist\n");
-            scanf("%s", file);
-            if(check_files(file) == 1){
-                printf("File exist\n");
-            }else{
-                printf("File does not exist\n");
-            }
-        }else{
-            printf("\nEnter  filename to load it into memory\n");
-            scanf("%s", file);
-            read_file(file);
-            printf("\nLoaded\n");
-        }
+    char filename[30] = "student.txt";
+    read_file(filename);
+    name_sort();
+    st_list *temp = student_l;
+    while(temp != NULL){
+        printf("%s %d\n", temp->name, temp->mark);
+        temp = temp->next;
     }
-    return 0;
 }
